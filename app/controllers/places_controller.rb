69b541aa507ec6de49d9bfa12f3e7cb0870135e8
Samuel Appling <sappling01@gmail.com>
2019-05-18 19:36:34 +0000

    Created a create page for new places to be added to.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,16 @@ class PlacesController < ApplicationController
   end
   
   def new
-    @place = place.new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def place_params
+    params.require(:place).permit(:name,:description, :address)
   end
 
 end
