ca248a22e41301b0e03e4734bfb756a93a2506d7
Samuel Appling <sappling01@gmail.com>
2019-06-02 16:43:39 +0000

    Im working out an issue with my heroku right now.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+      @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -28,11 +32,16 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
 
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -42,8 +51,11 @@ class PlacesController < ApplicationController
       return render plain: 'Not Allowed', status: :forbidden
     end
     
-    @place.destroy
-    redirect_to root_path
+      if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def place_params
